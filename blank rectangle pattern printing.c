c023e39ae66459ff2e6768644a51b7afac5e228c
7385481544 <144201526+7385481544@users.noreply.github.com>
2023-09-30 14:39:32 +0530

    Add files via upload

---
+++ blank rectangle pattern printing.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=0,j=0,x=0;
+   printf("enter a row and column size");
+   scanf("%d",&x);
+   printf("---------***-------\n");
+   for(i=1;i<=x;i++)
+  {
+    for(j=1;j<=x;j++)
+    {
+      if( j==1 || i==x || j==x ||i==1)
+     {
+      printf(" * ");
+     }
+     else
+     {
+      printf("   ");
+     }
+    }
+    printf("\n");
+  }
+  printf("---------***--------");
+  getch();
+  return 0;
+}
