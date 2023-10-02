0268825abf385db1d58d51efdfb7ad37f69a90e7
7385481544 <144201526+7385481544@users.noreply.github.com>
2023-10-02 12:35:51 +0530

    Add files via upload

---
+++ x pattern printing.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i=0,j=0,x=0;
+   printf("enter a row and column size");
+   scanf("%d",&x);
+   printf("---------***-------\n");
+   for(i=1;i<=x;i++)
+  {
+    for(j=1;j<=x;j++)
+    {
+      if( i==j || i+j==x+1)
+     {
+      printf(" * ");
+     }
+     else
+     {
+      printf("   ");
+     }
+    }
+    printf("\n");
+  }
+  printf("---------***--------");
+  getch();
+  return 0;
+}
